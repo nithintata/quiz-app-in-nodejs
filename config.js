e42cb40274e4abb1016d7573d84f2829e57bb3d9
Restyled.io <commits@restyled.io>
2020-06-08 02:36:47 +0000

    Restyled by clang-format

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': process.env.MONGO_URL
+  'secretKey' : '12345-67890-09876-54321',
+  'mongoUrl' : process.env.MONGO_URL
 }
