b1e16670bfda568a2551bed5ba79fb4a56fc888f
Restyled.io <commits@restyled.io>
2020-12-23 13:07:52 +0000

    Restyled by prettier

--- app.js
+++ app.js
@@ -1,68 +1,69 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const winston = require('./winston');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const xss = require('xss-clean');
-const hpp = require('hpp');
-const cors = require('cors');
-const compression = require('compression');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const winston = require("./winston");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cors = require("cors");
+const compression = require("compression");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const quizRouter = require('./routes/quizRouter');
-const config = require('./config');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const quizRouter = require("./routes/quizRouter");
+const config = require("./config");
 
-const mongoose = require('mongoose');
-const Quizes = require('./models/quizes');
+const mongoose = require("mongoose");
+const Quizes = require("./models/quizes");
 
 const DB = config.mongoUrl;
 
-mongoose.set('autoIndex', true);
+mongoose.set("autoIndex", true);
 
 const connectDB = async () => {
   const con = await mongoose.connect(DB, {
-    useNewUrlParser : true,
-    useCreateIndex : true,
-    useFindAndModify : false,
-    useUnifiedTopology : true,
-    autoIndex : true
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    autoIndex: true,
   });
   console.log(
-      chalk.bgGreen.black(`MongoDB Connected: ${con.connection.host}.`));
+    chalk.bgGreen.black(`MongoDB Connected: ${con.connection.host}.`)
+  );
 };
 
 connectDB();
 
 var app = express();
 
-app.enable('trust proxy');
+app.enable("trust proxy");
 
 // Set Body parser, reading data from body into req.body
-app.use(express.json({limit : '10kb'}));
-app.use(express.urlencoded({extended : true, limit : '10kb'}));
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
 // Set security HTTP headers
 app.use(helmet());
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(morgan('combined', {stream : winston.stream}));
+app.use(morgan("combined", { stream: winston.stream }));
 
-app.use(cookieParser('12345-67890'));
+app.use(cookieParser("12345-67890"));
 
 // Limit requests from the same API
 const limiter = rateLimit({
-  max : 100,
-  windowMs : 60 * 60 * 1000,
-  messege : 'Too many requests from this IP, Please try again in an hour!'
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  messege: "Too many requests from this IP, Please try again in an hour!",
 });
-app.use('/', limiter);
+app.use("/", limiter);
 
 // Data sanitization against XSS
 app.use(xss());
@@ -73,33 +74,38 @@ app.use(hpp());
 // Implement CORS
 app.use(cors());
 
-app.options('*', cors());
+app.options("*", cors());
 
 app.use(compression());
 
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/quizes', quizRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/quizes", quizRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) { next(createError(404)); });
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${
-      req.method} - ${req.ip}`);
+  winston.error(
+    `${err.status || 500} - ${err.message} - ${req.originalUrl} - ${
+      req.method
+    } - ${req.ip}`
+  );
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
